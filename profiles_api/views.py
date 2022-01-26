8b6c59b794ab50078073bdcd9fff2b7218457ec0
shreyas0906 <shreyas0906@gmail.com>
2022-01-26 21:43:29 +0530

    added put patch delete post methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -37,3 +37,21 @@ class HelloApiView(APIView):
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST) # can also send 400
+
+    def put(self, request, pk=None):
+        """
+        Handle updating an object
+        """
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """
+        Handle a partial update of an object
+        """
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """
+        Delete an object
+        """
+        return Response({'Method': 'DELETE'})
